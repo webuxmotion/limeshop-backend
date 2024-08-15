0c07f6efad711ce12cc412ce624741c6a9918cac
webuxmotion <pereverziev.andrii@gmail.com>
2024-08-15 23:30:42 +0300

    Popover

---
+++ pages/products/delete.js/[id].js
@@ -0,0 +1,25 @@
+import Layout from "@/components/Layout";
+import ProductForm from "@/components/ProductForm";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function Delete(params) {
+    const router = useRouter();
+    const { id } = router.query;
+    const [productInfo, setProductInfo] = useState(null);
+
+    useEffect(() => {
+        if (id) {
+            axios.get(`/api/products?id=${id}`).then(res => {
+                setProductInfo(res.data);
+            })
+        }
+        
+    }, [id]);
+
+    return <Layout>
+        <h1>Edit Product</h1>
+        <ProductForm data={productInfo} />
+    </Layout>
+}
